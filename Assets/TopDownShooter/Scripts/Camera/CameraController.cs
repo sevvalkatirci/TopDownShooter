60116fcd4820a773347929afa533a52d15e6bba1
sevvalkatirci <sevvalkatirci99@gmail.com>
2021-06-16 17:44:39 +0300

    Camera movement and third party assets added

---
+++ Assets/TopDownShooter/Scripts/Camera/CameraController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace TopDownShooter
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private CameraSettings _cameraSettings;
+        [SerializeField] private Transform _targetTransform;
+        [FormerlySerializedAs("_cameraTRansform")] [SerializeField] private Transform _cameraTransform;
+
+        private void Update()
+        {
+           CameraRotationFollow();
+           CameraMovementFollow();
+        }
+
+        private void CameraRotationFollow()
+        {
+            _cameraTransform.rotation=Quaternion.Lerp(_cameraTransform.rotation,
+                Quaternion.LookRotation(_targetTransform.position-_cameraTransform.position),
+                Time.deltaTime*_cameraSettings.RotationLerpSpeed);
+        }
+
+        private void CameraMovementFollow()
+        {
+            _cameraTransform.position = Vector3.Lerp(_cameraTransform.position,
+                _targetTransform.position + _cameraSettings.PositionOffset,
+                Time.deltaTime * _cameraSettings.PositionLerp);
+        }
+    }
+
+}
---
+++ Assets/TopDownShooter/Scripts/Camera/CameraSettings.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TopDownShooter
+{
+    [CreateAssetMenu(menuName = "TopDown Shooter/Camera/CameraSettings")]
+    public class CameraSettings : ScriptableObject
+    {
+        [Header("Rotation")]
+        [SerializeField] private float _rotationLerpSpeed = 1;
+        public float RotationLerpSpeed {get { return _rotationLerpSpeed; } }
+
+        [Header("Position")] 
+        [SerializeField] private Vector3 _positionOffset;
+        public Vector3 PositionOffset
+        {
+            get { return _positionOffset; }
+        }
+
+        [SerializeField] private float _positionLerp = 1f;
+        public float PositionLerp
+        {
+            get { return _positionLerp; }
+        }
+
+    }
+    
+}
+
